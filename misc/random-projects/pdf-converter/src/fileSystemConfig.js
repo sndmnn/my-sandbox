267564ffd806e3c975fa09ab879845a0a8bf8f1b
Henrique <sndmnn.dev@gmail.com>
2022-10-10 10:22:34 -0300

    added project

---
+++ misc/random-projects/pdf-converter/src/fileSystemConfig.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+const inDir = path.resolve(__dirname, '..', 'tmp', 'in');
+
+const outDir = path.resolve(__dirname, '..', 'tmp', 'out');
+
+module.exports = {
+  inDir,
+  outDir,
+};
